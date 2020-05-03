290f9bc689e618cc82875c574f48a1ba39c77f2e
Your Name <you@example.com>
2020-05-03 16:18:01 +0300

    bom

---
+++ BOM/bom.js
@@ -0,0 +1,8 @@
+document.getElementById('demo').innerHTML ="browser width is:"+window.innerWidth;
+document.getElementById('demo1').innerHTML ="browser height is:"+window.innerHeight;
+
+let c = document.createElement('p');
+c.innerHTML= window.screen.width;
+
+let hu = document.querySelector('.yui');
+alert(hu.textContent);
